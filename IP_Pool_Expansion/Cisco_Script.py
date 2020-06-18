19aaaf25a827f869b0b66f85613a23a0833612c5
AhAymann <ahmed.ayman-awad@vodafone.com>
2020-06-18 11:56:51 +0200

    Merge branch 'master' of https://github.com/NourAttya/Corporate_APN_Project
    # Conflicts:
    #	GUI.py

--- IP_Pool_Expansion/Cisco_Script.py
+++ IP_Pool_Expansion/Cisco_Script.py
@@ -2,8 +2,15 @@
 def Cisco_Script (Contextname,Ippoolarray,netmaskpoolarray,Subnetnames,MTX,SorD,pathToSave,TypeOfAPN,APNname):
 
     script = open(pathToSave + Contextname + "_Script.txt", "a")
-    script.write('On' + MTX + '-GGSN'  + ':\n')
+<<<<<<< Updated upstream
+    script.write('On ' + MTX + '-GGSN'  + ':\n')
     script.write(' Configure\n')
+=======
+    script.write('On  ' + MTX + '-GGSN'  + ':\n')
+    script.write('==============\n \n')
+    script.write('save configuration /flash/configfiles/Before_' + Contextname + '.cfg -redundant\n')
+    script.write(' Configure\n ')
+>>>>>>> Stashed changes
     script.write( Contextname + '\n')
 
     if (TypeOfAPN == 'PC Connectivity'):
@@ -37,12 +44,36 @@ def Cisco_Script (Contextname,Ippoolarray,netmaskpoolarray,Subnetnames,MTX,SorD,
                     netmaskpoolarray[i]) + ' private 0 group-name internet advertise-if-used address-hold-timer 60 \n')
 
     elif (TypeOfAPN == 'Internet'):
-        for i in range (len(Ippoolarray)):
+        if (SorD == "Static"):
+            for i in range (len(Ippoolarray)):
                 script.write(
-                 'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
-                     netmaskpoolarray[i]) + ' private 0 group-name ' + APNname + '_pool advertise-if-used \n')
+                    'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                    netmaskpoolarray[i]) + ' static group-name ' + APNname + '_pool advertise-if-used vrf \n')
+        else:
+             for i in range(len(Ippoolarray)):
+                script.write(
+                'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                    netmaskpoolarray[i]) + ' private 0 group-name ' + APNname + '_pool advertise-if-used \n')
 
     else :
         return
     script.write('  exit\n')
     script.write('    end\n')
+<<<<<<< Updated upstream
+    script.close()
+=======
+    script.write('\n')
+    script.write('\n')
+    script.write('save configuration /flash/configfiles/After_' + Contextname + '.cfg -redundant\n')
+    script.write('---------------------------------------------------------------\n')
+    script.write('\n')
+    script.write('Rollback\n')
+    script.write('=========\n')
+    script.write('\n')
+    script.write('config\n')
+    script.write(Contextname + '\n')
+    script.write(' no ip pool' + Subnetnames[i] + '\n')
+    script.write('  exit\n')
+    script.write('    end\n')
+    script.write('save configuration /flash/configfiles/After_Rollback_' + Contextname + '.cfg -redundant\n')
+>>>>>>> Stashed changes
