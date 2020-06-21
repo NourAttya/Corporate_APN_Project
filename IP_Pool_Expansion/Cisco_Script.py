9d6d6917d97731fb4c0d96f5edab76848c108315
AhAymann <ahmed.ayman-awad@vodafone.com>
2020-06-21 12:57:41 +0200

    Merge branch 'master' of https://github.com/NourAttya/Corporate_APN_Project
    # Conflicts:
    #	GUI.py

--- IP_Pool_Expansion/Cisco_Script.py
+++ IP_Pool_Expansion/Cisco_Script.py
@@ -14,42 +14,42 @@ def Cisco_Script (Contextname,Ippoolarray,netmaskpoolarray,Subnetnames,MTX,SorD,
         if (SorD == "Static"):
             for i in range (len(Ippoolarray)):
                 script.write(
-                    'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                    'ip pool ' + Subnetnames[i] +" " + str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' static group-name ' + APNname + '_pool advertise-if-used vrf' + APNname+'_vrf \n')
         else:
              for i in range(len(Ippoolarray)):
                 script.write(
-                'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                'ip pool ' + Subnetnames[i] +" " + str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' private 0 group-name ' + APNname + '_pool advertise-if-used vrf' + APNname+'_vrf\n')
 
     elif (TypeOfAPN == 'Sim2Sim'):
         for i in range(len(Ippoolarray)):
             script.write(
-                'ip pool ' + Subnetnames[i] + str(Ippoolarray[i]) + " " + str(
+                'ip pool ' + Subnetnames[i] +" "+ str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' static group-name ' + APNname + '_pool advertise-if-used vrf' + APNname+'_vrf \n')
 
     elif (TypeOfAPN == '3G WIC'):
         for i in range (len(Ippoolarray)):
                 script.write(
-                    'ip pool ' + Subnetnames[i] + str(Ippoolarray[i]) + " " + str(
+                    'ip pool ' + Subnetnames[i] +" "+ str(Ippoolarray[i]) + " " + str(
                         netmaskpoolarray[i]) + ' static group-name ' + APNname + '_pool advertise-if-used \n')
 
     elif (TypeOfAPN == 'Commerical_main_APN'):
         for i in range(len(Ippoolarray)):
             script.write(
-                'ip pool ' + Subnetnames[i] + str(Ippoolarray[i]) + " " + str(
+                'ip pool ' + Subnetnames[i] +" " + str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' private 0 group-name internet advertise-if-used address-hold-timer 60 \n')
 
     elif (TypeOfAPN == 'Internet'):
         if (SorD == "Static"):
             for i in range (len(Ippoolarray)):
                 script.write(
-                    'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                    'ip pool ' + Subnetnames[i] +" " + str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' static group-name ' + APNname + '_pool advertise-if-used vrf \n')
         else:
              for i in range(len(Ippoolarray)):
                 script.write(
-                'ip pool ' + Subnetnames[i]  + str(Ippoolarray[i]) + " " + str(
+                'ip pool ' + Subnetnames[i] +" " + str(Ippoolarray[i]) + " " + str(
                     netmaskpoolarray[i]) + ' private 0 group-name ' + APNname + '_pool advertise-if-used \n')
 
     else :
@@ -67,7 +67,7 @@ def Cisco_Script (Contextname,Ippoolarray,netmaskpoolarray,Subnetnames,MTX,SorD,
     script.write('config\n')
     script.write(Contextname + '\n')
     for i in range(len(Subnetnames)):
-        script.write(' no ip pool' + Subnetnames[i] + '\n')
+        script.write(' no ip pool ' + Subnetnames[i] + '\n')
     script.write('  exit\n')
     script.write('    end\n')
     script.write('save configuration /flash/configfiles/After_Rollback_' + Contextname + '.cfg -redundant\n')
