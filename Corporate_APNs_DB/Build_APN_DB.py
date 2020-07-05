e03a64a6cae89f2937fc13675b9f6c8a8b195818
NourAttya <nourhaneattya@hotmail.com>
2020-07-05 12:05:57 +0200

    05-07-2020

--- Corporate_APNs_DB/Build_APN_DB.py
+++ Corporate_APNs_DB/Build_APN_DB.py
@@ -1,4 +1,6 @@
-#Nour Attya
+
+import os
+
 def getAllAPNS(configPath):
 #input : GGSN Configuration file
 #output : 4 Lists with the same length
@@ -10,50 +12,80 @@ def getAllAPNS(configPath):
     contextName=[]
     VRF=[]
     interface=[]
-    with open(configPath) as f:
-        reading = 0
-        line = f.readline()
-        while (line):
+    for filename in os.listdir(configPath):
+        fullnameConfigFile = os.path.join(configPath, filename)
+        with open(fullnameConfigFile) as f:
+            reading = 0
             line = f.readline()
-            line=line.lstrip()
-            if (line.startswith("apn ") and not line.startswith("apn schema")):
-                reading=1
-                if(len(APNName)!=len(contextName)): #Why this?
-                    contextName.append(None)
+            while (line):
+                line = f.readline()
+                line=line.lstrip()
+                if (line.startswith("apn ") and not line.startswith("apn schema")):
+                    reading=1
+                    if(len(APNName)!=len(contextName)):
+                        contextName.append(None)
 
-                VRF.append(0)
-                interface.append(0)
-                APNName.append(line.split(" ")[1].replace("\n",""))
-                continue
-            if(reading==1):
-                if(line.startswith("ip context-name")):
-                    contextName.append(line.split(" ")[2].replace("\n",""))
-                if("vrf" in line):
-                    name=line.split(" ")[2] #[2]to be revised
-                    check = [name.find(i) for i in APNName]
-                    if(check.count(0)!=0):
-                        index=check.index(0)
-                        VRF[index]=1
-                if ("interface" in line):
-                    name = line.split(" ")[1]
-                    check = [name.find(i) for i in APNName]
-                    if (check.count(0) != 0):
-                        for i in range(len(check)):
-                            if(check[i]==0):
-                                interface[i] = 1
-
-
-
-
-    print("APNName",APNName)
-    print("contextName",contextName)
-    print("VRF",VRF)
-    print("interface",interface)
+                    VRF.append(0)
+                    interface.append(0)
+                    APNName.append(line.split(" ")[1].replace("\n",""))
+                    continue
+                if(reading==1):
+                    if(line.startswith("ip context-name")):
+                        contextName.append(line.split(" ")[2].replace("\n",""))
+                    if("vrf" in line):
+                        name=line.split(" ")[2]
+                        check = [name.find(i) for i in APNName]
+                        if(check.count(0)!=0):
+                            index=check.index(0)
+                            VRF[index]=1
+                    if ("interface" in line):
+                        name = line.split(" ")[1]
+                        check = [name.find(i) for i in APNName]
+                        if (check.count(0) != 0):
+                            for i in range(len(check)):
+                                if(check[i]==0):
+                                    interface[i] = 1
+        print("APNName",len(APNName)APNName)
+        print("contextName",len(contextName))
+        print("VRF",len(VRF))
+        print("interface",len(interface))
 
 
+    return APNName,contextName,VRF,interface
 
 
+def getAPNType(APNName,contextName,VRF,interface):
+    APNType=[]
 
-#C:\Users\V19AAwad2\Desktop\Session Logs\GGSN dump 29 1
-getAllAPNS("C:\\Users\\V19AAwad2\\Desktop\\Session Logs\\GGSN dump 29 1\\TG2 GGSN 21 6 2020")
-#getAllAPNS("D:\\Automation Team\\Corporate APN Project\\config files\\TG2 GGSN 21 6 2020")
+    for i in range(len(APNName)):
+        if(VRF[i]==1 and interface[i]==0):
+            APNType.append("sim2sim")
+        elif(VRF[i]==1 and interface[i]==1):
+            APNType.append("PC Connectivity")
+        elif(VRF[i]==0 and contextName[i]=="Gi-Corp"):
+            APNType.append("3G WIC")
+        elif (VRF[i]==0 and contextName[i]=="VAS-Corp"):
+            APNType.append("Internet")
+        else:
+            APNType.append(str(contextName[i])+" "+str(VRF[i])+" "+str(interface[i]))
+
+    return APNType
+
+
+def writeInCSV(APNName,APNType,pathToSave):
+
+    with open(pathToSave + '\\Corporate APNs' + '.csv', 'w') as out_file:
+        out_file.write('{0},{1}\n'.format("APN Name", "APN Type"))
+        for i in range(len(APNName)):
+            out_file.write('{0},{1}\n'.format(APNName[i],APNType[i]))
+
+
+def APNDB(configPath):
+    pathToSave=configPath
+    APNName, contextName, VRF, interface=getAllAPNS(configPath)
+    APNType=getAPNType(APNName,contextName,VRF,interface)
+    writeInCSV(APNName,APNType,pathToSave)
+
+# # getAllAPNS("D:\\Automation Team\\Corporate APN Project\\config files\\TG2 GGSN 21 6 2020")
+# # # writeInCSV(["Test1","Test2","Test3"],["3GWIc","PC Connectivity","Internet"],"D:\\Automation Team\\Corporate APN Project\\")
+# # APNDB("D:\\Automation Team\\Corporate APN Project\\config files\\TG2 GGSN 21 6 2020","D:\\Automation Team\\Corporate APN Project\\")
