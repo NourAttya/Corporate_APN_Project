98987acc4dd8aca91145d762fb1e1b747f97580d
AhAymann <ahmed.ayman-awad@vodafone.com>
2020-08-12 14:46:48 +0200

    hii

---
+++ Security Blocking IPs/Blocking_IPs_DC.py
@@ -0,0 +1,32 @@
+def Blocking_IPs_DC (BlockingIP):
+    script = open(BlockingIP + "_Script.txt", "w")
+    script.write('HQ-ON: \n')
+    script.write('------ \n \n')
+    script.write('set address "Internet" "Suspected_' +BlockingIP+ ' '+BlockingIP+' 255.255.255.255 \n')
+    script.write('set group address "Internet" "Suspected_IPs_1" add "Suspected_'+BlockingIP+'" \n')
+    script.write(' \n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('RMD-ON: \n')
+    script.write('------ \n \n')
+    script.write('set address "Internet" "Suspected_' + BlockingIP + ' ' + BlockingIP + ' 255.255.255.255 \n')
+    script.write('set group address "Internet" "Suspected_IPs_1" add "Suspected_' + BlockingIP + '" \n')
+    script.write(' \n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('################################################################################### \n \n')
+
+    script.write('ROLLBACK: \n')
+    script.write('HQ-ON: \n')
+    script.write('------ \n \n')
+    script.write('unset address "Internet" "Suspected_' + BlockingIP + ' ' + BlockingIP + ' 255.255.255.255 \n')
+    script.write('unset group address "Internet" "Suspected_IPs_1" remove "Suspected_' + BlockingIP + '" \n')
+    script.write(' \n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('RMD-ON: \n')
+    script.write('------ \n \n')
+    script.write('unset address "Internet" "Suspected_' + BlockingIP + ' ' + BlockingIP + ' 255.255.255.255 \n')
+    script.write('unset group address "Internet" "Suspected_IPs_1" remove "Suspected_' + BlockingIP + '" \n')
+    script.write(' \n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
---
+++ Security Blocking IPs/Blocking_IPs_MI.py
@@ -0,0 +1,80 @@
+def Blocking_IPs_MI (BlockingIP):
+    script = open(BlockingIP + "_Script.txt", "w")
+    script.write('HQ-GI: \n')
+    script.write('set security zones security-zone Internet address-book address Suspected_'+BlockingIP+' '+BlockingIP+'\n')
+    script.write('set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_'+BlockingIP+'\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('HQ-2nd Leg Gi: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('MKT-Gi: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('MNS-Gi: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('RMD-2nd GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('RMD-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('Alex-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('BS-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('BS 2nd Leg-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('CA4-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('CA5-GI: \n')
+    script.write(
+        'set security zones security-zone Internet address-book address Suspected_' + BlockingIP + ' ' + BlockingIP + '\n')
+    script.write(
+        'set security zones security-zone Internet address-book address-set Suspected_IPs_1 address Suspected_' + BlockingIP + '\n')
+    script.write('--------------------------------------------------------------------------------- \n \n')
+
+    script.write('################################################################################### \n \n')
+
+
