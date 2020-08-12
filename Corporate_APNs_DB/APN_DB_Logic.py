e9e82659d804f9bf2e594dff56f9141c40ddbd48
AhAymann <ahmed.ayman-awad@vodafone.com>
2020-08-12 11:56:33 +0200

    hii

--- Corporate_APNs_DB/APN_DB_Logic.py
+++ Corporate_APNs_DB/APN_DB_Logic.py
@@ -1,27 +1,70 @@
 import xlrd
-def APN_DB_Logic(From, To, MTX, IPpoolCount):
-    APNs = []
+def APN_DB_Logic(From, To):
+    #APNs = []
 
-    workbook = xlrd.open_workbook("D:\\EPC\\Automation\\Corporate_APN_Project_PullfromNour\\Corporate APNs.xlsx")
+    workbook = xlrd.open_workbook("D:\\EPC\\Automation\\Corporate_APN_Project\\TG1 Corporate APNs.xlsx")
     worksheet = workbook.sheet_by_index(0)
     print(worksheet)
     script = open('To be edited '+ "_Script.txt", "w")
-    script.write('On ' + MTX + '-GGSN' + ':\n')
+    row = worksheet.row_values(From)
+    script.write('On ' + row[4] + '-GGSN' + ':\n')
 
     for j in range (From, To):
         row = worksheet.row_values(j)
         print(row)
         if j == 0: continue
-        if row[1]=='3G WIC':
+        IPpoolCount = row[3]
+        if row[1]=='PC Connectivity':
             script.write(' Configure\n')
             script.write(' context aaa\n')
             script.write(' no ' + row[0] + ' \n')
             script.write(' exit \n')
             script.write(' context' + row[2] + '\n')
-            script.write(' no ip pool ' + IPpoolCount[i] '\n')
-            script.write(' ip pool ' + row[4] + '_pool.1 \n')
+            for i in range (1, int(IPpoolCount) +1):
+                script.write(' no ip pool_' + str(i) +'\n')
+            script.write(' sleep 2 \n')
+            script.write(' no ip route 0.0.0.0 0.0.0.0 0.0.0.0 ' + row[0] + 'vrf ' + row[0] +'_vrf\n')
+            script.write(' sleep 2 \n')
+            script.write(' no interface ' + row[0] + '\n')
+            script.write(' sleep 2 \n')
+            script.write(' no ip vrf ' + row[0] + '_vrf \n')
+            script.write(' sleep 2 \n')
+            script.write(' end \n')
 
+        elif row[1]=='3G WIC':
+            script.write(' Configure\n')
+            script.write(' context aaa\n')
+            script.write(' no ' + row[0] + ' \n')
+            script.write(' exit \n')
+            script.write(' context' + row[2] + '\n')
+            for i in range (1, int(IPpoolCount) +1):
+                script.write(' no ip pool_' + str(i) +'\n')
+            script.write(' end \n')
 
+        elif row[1] == 'Internet':
+            script.write(' Configure\n')
+            script.write(' context aaa\n')
+            script.write(' no ' + row[0] + ' \n')
+            script.write(' exit \n')
+            script.write(' context' + row[2] + '\n')
+            for i in range (1, int(IPpoolCount) +1):
+                script.write(' no ip pool_' + str(i) +'\n')
+            script.write(' end \n')
+
+        elif row[1] == 'sim2sim':
+            script.write(' Configure\n')
+            script.write(' context aaa\n')
+            script.write(' no ' + row[0] + ' \n')
+            script.write(' exit \n')
+            script.write(' context' + row[2] + '\n')
+            for i in range (1, int(IPpoolCount) +1):
+                script.write(' no ip pool_' + str(i) +'\n')
+            script.write(' sleep 2 \n')
+            script.write(' no ip vrf ' + row[0] +'_vrf \n')
+            script.write(' sleep 2 \n')
+            script.write(' end \n')
+
+        else: continue
 
 
      #    for i in range(len(row)): #Range el x and y
@@ -34,4 +77,4 @@ def APN_DB_Logic(From, To, MTX, IPpoolCount):
     #print(APNs)
 
 
-APN_DB_Logic(0,10)
+APN_DB_Logic(1,10)
